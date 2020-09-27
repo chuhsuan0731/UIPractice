7415976e8c2e09d27f58a4fb26fd478661766383
chuhsuan0731 <71374913+chuhsuan0731@users.noreply.github.com>
2020-09-27 17:27:09 +0800

    Update welcome.cpp

--- welcome/welcome.cpp
+++ welcome/welcome.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
 
+int main(int argc, char *argv[]) 
+{
+   if (argc < 2 || argc > 2) 
+   {
+       cout << "usage(使用方法):" << endl;
+       cout << " " << argv[0] << " user name" << endl;
+   }
+   else
+   {
+     cout << "Hello, " << argv[1] << "." << endl;
+     cout << "Welcome to the CHUHSUAN's world." << endl;
+   }
+return 0;
+}
