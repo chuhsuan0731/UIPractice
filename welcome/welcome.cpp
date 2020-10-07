aed9400b35c1a64523e7284db9e93daf6076554e
chuhsuan0731 <71374913+chuhsuan0731@users.noreply.github.com>
2020-10-07 10:16:48 +0800

    Update welcome.cpp

--- welcome/welcome.cpp
+++ welcome/welcome.cpp
@@ -1,17 +1,62 @@
 #include <iostream>
+
 using namespace std;
 
-int main(int argc, char *argv[]) 
-{
-   if (argc < 2 || argc > 2) 
-   {
-       cout << "usage(使用方法):" << endl;
-       cout << "   " << argv[0] << " user name" << endl;
-   }
-   else
-   {
-     cout << "Hello, " << argv[1] << "." << endl;
-     cout << "Welcome to the CHUHSUAN's world." << endl;
-   }
-return 0;
+void error(const string& msg) {
+    cerr << "Error:\n  " << msg << endl;
+}
+
+void usage(const string& cmd) {
+    string opt[] = {
+        "where\n",
+        " -h host      the host name.\n",
+        " -v visitor   the guest name.\n",
+        " -h help      the usage.\n",
+    };
+   
+   
+    for (int i = 0; i < (sizeof(opt) / sizeof(string)); i++) {
+        cout << opt[i];
+    }
+
+    exit(-1);
+}
+
+if (arg =="-h"){
+      cout << "Usage(輸入方法）: \n";
+      cout << "  " << cmd << " -h host（主人姓名） -v visitor（拜訪者姓名）\n";
+
+
+void welcome(const string& host, const string& guest) {
+    cout << "Hello, " << guest << ".\n";
+    cout << "This is " << host << ", welcome.\n";
+}
+
+int main(int argc, char *argv[]) {
+    string host;
+    string guest;
+
+    if (argc != 5) {
+        error("Wrong number of parameters.");
+        usage(argv[0]);
+    }
+
+    for (int i = 1; i < argc; i = i + 2) {
+        string arg = argv[i];
+
+        if (arg == "-h") {
+            host = argv[i + 1];
+        }
+        else if (arg == "-v") {
+            guest = argv[i + 1];
+        }
+        else {
+            error("Unknown options: " + arg);
+            usage(argv[0]);
+        }
+    }
+
+    welcome(host, guest);
+
+    return 0;
 }
